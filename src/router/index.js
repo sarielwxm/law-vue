56c1a2fc7f04725a532d2ec5c9d7ed6c4f183f95
sarielwxm <40795114+sarielwxm@users.noreply.github.com>
2020-01-13 16:11:21 +0800

    拦截器

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// 导入刚才编写的组件
+// 导入编写的组件
 import AppIndex from '@/components/home/AppIndex'
 import Login from '@/components/Login'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     // 下面都是固定的写法
     {
@@ -17,7 +18,10 @@ export default new Router({
     {
       path: '/index',
       name: 'AppIndex',
-      component: AppIndex
+      component: AppIndex,
+      meta: {
+        requiredAuth: true
+      }
     }
   ]
 })
